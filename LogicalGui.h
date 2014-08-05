3412e9e539938adef7648698f09ec2526e8602b4
Jan Dalheimer <jan@dalheimer.de>
2014-08-05 20:15:42 +0200

    Setup travis-ci

--- LogicalGui.h
+++ LogicalGui.h
@@ -24,8 +24,12 @@
 
 #if QT_VERSION == QT_VERSION_CHECK(5, 2, 0)
 #include <5.2.0/QtCore/private/qobject_p.h>
+#elif QT_VERSION == QT_VERSION_CHECK(5, 2, 1)
+#include <5.2.1/QtCore/private/qobject_p.h>
 #elif QT_VERSION == QT_VERSION_CHECK(5, 3, 0)
 #include <5.3.0/QtCore/private/qobject_p.h>
+#else
+#error Please add support for this version of Qt
 #endif
 
 class Bindable
